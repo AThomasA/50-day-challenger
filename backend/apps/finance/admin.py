fd40bc6741db1e616a6064670ada07a4082c962b
Ath0maz <allan.thomas.a.o@gmail.com>
2026-01-23 11:22:22 -0300

    feat:configure revenue and expense in admin, make migrations and create superuser to manage manually

--- backend/apps/finance/admin.py
+++ backend/apps/finance/admin.py
@@ -1,3 +1,17 @@
 from django.contrib import admin
+from .models import Revenue, Expense
 
-# Register your models here.
+@admin.register(Revenue)
+class RevenueAdmin(admin.ModelAdmin):
+    list_display = ("name", "amount", "received_date", "create_at",)
+    list_filter = ("received_date", "create_at",)
+    search_fields = ("name",)
+    ordering = ("-received_date",)
+    
+@admin.register(Expense)
+class ExpenseAdmin(admin.ModelAdmin):
+    list_display = ("name", "amount", "payment_date", "create_at",)
+    list_filter = ("payment_date", "create_at",)
+    search_fields = ("name",)
+    ordering = ("-payment_date",)
+
